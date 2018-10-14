efcf3939e2b18cad3a2f59f796bf929eed69548c
Digital <josephpro42@live.co.uk>
2018-10-14 02:36:14 +0100

    Add email link

--- assets/js/social-icon.js
+++ assets/js/social-icon.js
@@ -12,10 +12,10 @@ $("*[data-icon-text]").hover(
     var text = $(this).data("icon-text");
     var position = $(this).position();
     var sbHeight = $(".sidebar").position().top;
-
+    
     $("#icon-text").html(text);
     $("#icon-text").css("top", sbHeight + position.top - 22.5 + "px");
-    $("#icon-text").css("left", position.left + 65 + "px");
+    $("#icon-text").css("left", position.left + 70 + "px");
 
     $("#icon-text").css("visibility", "visible");
     $("#icon-text").css("opacity", "1");
