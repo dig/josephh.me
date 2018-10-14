14b99ae82f5830f5247f493ff99861ac37dc5876
Digital <josephpro42@live.co.uk>
2018-10-14 01:54:13 +0100

    Start design

---
+++ assets/js/social-icon.js
@@ -0,0 +1,21 @@
+
+/*
+ * Handles the text that appears when you hover
+ * over the social media icons.
+ */ 
+
+$("*[data-icon-text]").hover(
+  function() {
+    var text = $(this).data("icon-text");
+    var position = $(this).position();
+    var sbHeight = $(".sidebar").position().top;
+
+    $("#icon-text").html(text);
+    $("#icon-text").css("top", sbHeight + position.top - 22.5 + "px");
+    $("#icon-text").css("left", position.left + 65 + "px");
+
+    $("#icon-text").css("display", "");
+  }, function() {
+    $("#icon-text").css("display", "none");
+  }
+);
